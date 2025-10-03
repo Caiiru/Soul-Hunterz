a375c70043c019d8947912e3547173389d4d5d08
Caiiru <54562856+Caiiru@users.noreply.github.com>
2025-10-03 17:10:44 -0300

    FEAT: Melee Dummy Attacking

---
+++ Xp6Game/Assets/Entities/Enemies/DummyEnemy/Melee_DummyEnemy.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Melee_DummyEnemy : Enemy
+{
+    public float attackCooldown = 5f;
+    private float _attackTimer = 0f;
+    private bool _canAttack = false;
+
+    [Header("VFX")]
+    public Transform SwordJoint;
+
+    private Animator _animator;
+    public List<SlashVFX> slashes;
+    public int currentSlash = 0;
+    protected override void Start()
+    {
+        base.Start();
+        _animator = GetComponent<Animator>();
+        VFXDebugManager.OnInputPressed += OnInputPressed;
+
+    }
+
+    private void OnInputPressed(int key)
+    {
+        if (key == 0)
+        {
+            _canAttack = true;
+        }
+        if (key == 1)
+        {
+            _canAttack = false;
+        }
+    }
+
+    // Update is called once per frame
+
+    protected override void Update()
+    {
+        base.Update();
+        HandleTimer();
+    }
+
+    private void HandleTimer()
+    {
+
+        if (!_canAttack) return;
+        _attackTimer += Time.deltaTime;
+        if (_attackTimer >= attackCooldown)
+        {
+            Attack();
+            _attackTimer = 0;
+        }
+
+    }
+    void Attack()
+    {
+        _animator.SetTrigger("Attack");
+
+    }
+
+    IEnumerator SpawnSlash()
+    {
+        GameObject _slash = Instantiate(slashes[currentSlash].SlashPrefab, transform.position, SwordJoint.rotation);
+        _slash.GetComponentInChildren<VisualEffect>().Stop();
+        yield return new WaitForSeconds(slashes[currentSlash].delay);
+        _slash.GetComponentInChildren<VisualEffect>().Play();
+        Destroy(_slash, 2f);
+        
+    }
+
+}
+
+[System.Serializable]
+public class SlashVFX
+{
+    public GameObject SlashPrefab;
+    public float delay;
+}
