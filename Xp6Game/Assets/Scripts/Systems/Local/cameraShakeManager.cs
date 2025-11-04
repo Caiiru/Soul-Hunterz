b914572415e6a49a08799fab0e68119481a12372
CaioGui <caioanimationdesign@gmail.com>
2025-11-04 16:29:20 -0300

    test cinemachine screenshake

---
+++ Xp6Game/Assets/Scripts/Systems/Local/cameraShakeManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using Unity.Cinemachine;
+using Unity.Cinemachine.Editor;
+
+
+public class cameraShakeManager : MonoBehaviour
+{
+    public static cameraShakeManager instance;
+    [SerializeField] private float globalShakeForce = 1f;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        
+    }
+
+    public void CameraShake(CinemachineImpulseSource Source)
+    {
+        Source.GenerateImpulseWithForce(globalShakeForce);
+    }
+
+}
