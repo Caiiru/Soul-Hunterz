5dfdf4ed49ccab6f31c2801589451eb839be2b74
Caiiru <54562856+Caiiru@users.noreply.github.com>
2025-11-04 17:50:24 -0300

    feat: minion

---
+++ Xp6Game/Assets/Entities/Enemies/Melee/Minion/MinionSounds.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MinionSounds : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void PlaySound()
+    {
+        Debug.Log("play sound minion");
+    } 
+}
