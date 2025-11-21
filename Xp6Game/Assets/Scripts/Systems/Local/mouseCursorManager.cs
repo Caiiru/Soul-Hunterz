43aac81cf375f696609aafde2f38b9b270d6dffa
CaioGui <caioanimationdesign@gmail.com>
2025-11-21 16:19:35 -0300

    new cursor sprites

--- Xp6Game/Assets/Scripts/Systems/Local/mouseCursorManager.cs
+++ Xp6Game/Assets/Scripts/Systems/Local/mouseCursorManager.cs
@@ -1,21 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class mouseCursorManager : MonoBehaviour
+
+
+public class CursorController : MonoBehaviour
 {
-    [SerializeField] private Camera mainCamera;
 
-    void Start()
-    {
-        Cursor.visible = false;
+     public Texture2D cursor;
+
+     public Texture2D cursorClicked;
+
+     public InputActionReference Click;
+
+
+     private void Awake() {
+    
+         ChangeCursor(cursor);
+         Cursor.lockState = CursorLockMode.Confined;
+   }
+
+
+
+    private void Start() {
+        Click.action.performed += _ => StartedClick();
+        Click.action.canceled += _ => EndedClick();
     }
 
-    void Update()
-    {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            transform.position = hit.point;
-            transform.rotation = mainCamera.transform.rotation;
-        }
+    private void StartedClick() {
+        ChangeCursor(cursorClicked);
     }
+
+    private void EndedClick() {
+        ChangeCursor(cursor);
+        Debug.Log("Cursor Released");
+    }
+
+
+
+    private void ChangeCursor(Texture2D cursorType) {
+        Vector2 hotSpot = new Vector2(cursorType.width / 2, cursorType.height / 2);
+        Cursor.SetCursor(cursorType, hotSpot, CursorMode.Auto);
+   }
 }
